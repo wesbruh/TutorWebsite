7d375765ab543a428eacd4142508a022447a69b5
wesbruh <westleyv77@gmail.com>
2024-11-01 17:51:50 -0700

    init the project

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect()
+
+app.listen(3001, () => {
+    console.log("SERVER RUNS PERFECTLY!");
+});
