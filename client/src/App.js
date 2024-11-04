67950975834b798128cc362f458c22bdd511cf24
Jagroop <jagroopsingh1936@gmail.com>
2024-11-03 22:56:58 -0800

    Created Login folder containing Student login implementation. Created global style sheet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 
 import './App.css';
+import { StudentLoginPage } from './Login/indexStudentLogin';
 
 function App() {
-
+  return(
+    <div>
+      <StudentLoginPage></StudentLoginPage>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Login/indexStudentLogin.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+/* IMAGES NEED TO BE SUPPORTED, JUST TESTING RIGHT NOW - JAGROOP*/
+/*import { Frame } from "./Frame";*/
+/*import backButton from "./back-button.png";*/
+/*import greenRectangleDesign from "./green-rectangle-design.png";*/
+/*import logo from "./logo.png";*/
+import "./styleStudentLogin.css"; // Modified
+
+export const StudentLoginPage = () => {
+    return (
+        <div className="student-login-page">
+            <div className="div">
+                <div className="overlap-group">
+                    {/*<img
+                        className="green-rectangle"
+                        alt="Green rectangle"
+                        src={greenRectangleDesign}
+                    />*/}
+
+                    <div className="sign-in-box">
+                        <div className="div-2">
+                            <div className="frame-2">
+                                <div className="text-wrapper">Student Login</div>
+
+                                <div className="frame-3">
+                                    <div className="frame-4">
+                                        {/*<Frame className="frame-instance" />*/}
+                                        <input className="input" />
+                                    </div>
+
+                                    <div className="sign-in-button">
+                                        <div className="text-wrapper-2">Sign in</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="text-wrapper-3">Forgot your password?</div>
+                        </div>
+
+                        <div className="frame-wrapper">
+                            <div className="frame-5">
+                                <div className="rectangle" />
+
+                                <div className="text-wrapper-4">Tutor Login</div>
+
+                                <div className="rectangle" />
+                            </div>
+                        </div>
+
+                        <div className="div-2">
+                            <p className="text-wrapper">No account? Sign up as a:</p>
+
+                            <div className="sign-up-buttons">
+                                <div className="div-wrapper">
+                                    <div className="text-wrapper-5">Student</div>
+                                </div>
+
+                                <div className="div-wrapper">
+                                    <div className="text-wrapper-5">Tutor</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="top-bar">
+                    <div className="back-logo">
+                        {/*<img className="logo" alt="Logo" src={logo} />
+
+                        <img className="back-button" alt="Back button" src={backButton} />*/}
+                    </div>
+
+                    <p className="TUTOR-TITANS">
+                        <span className="span">TUTOR</span>
+
+                        <span className="text-wrapper-6">&nbsp;</span>
+
+                        <span className="text-wrapper-7">TITANS</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
