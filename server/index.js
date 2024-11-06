210bf3e2d3d56f3a991bf6d7d117e8934481f4e0
chikpan <chikpanwong@csus.edu>
2024-11-06 10:42:40 -0800

    updata server and add base register

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-const express = require("express")
-const app = express()
-// Come back to this - Jay
-//const mongoose = require('mongoose')
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
-// mongoose.connect()
+//configure env
+dotenv.config();
 
-app.listen(3001, () => {
-    console.log("SERVER RUNS PERFECTLY!");
+//databse config
+connectDB();
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app. listen (PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
