d3429f7edbc9b44c1c81b768048ffd43c3d1b1ad
chikpan <chikpanwong@csus.edu>
2024-11-08 18:14:41 -0800

    added base login

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,10 +14,6 @@ const userSchema = new mongoose. Schema ({
     password: {
         type:String,
         required: true
-    },
-    Bio: {
-        type:String,
-        required: true
     }
 },{timestamps:true})
 export default mongoose.model ('users', userSchema)
