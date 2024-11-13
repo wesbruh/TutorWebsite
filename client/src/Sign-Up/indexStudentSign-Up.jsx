9a9e9d70c90e715a42c6e8018a4dcc889ce822e2
chikpan <chikpanwong@csus.edu>
2024-11-13 11:14:59 -0800

    fix sign up page header

--- client/src/Sign-Up/indexStudentSign-Up.jsx
+++ client/src/Sign-Up/indexStudentSign-Up.jsx
@@ -1,146 +1,151 @@
-import React, {useState}from "react";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import backButton from "../assets/BackButton.png";
 import greenRectangleDesign from "../assets/greenbox.png";
 import logo from "../assets/logo.svg";
 import "./styleStudentSign-Up.css";
 
 export const StudentSignUp = () => {
-    const navigate = useNavigate();
-    const tutorSignup = () =>{
-        navigate('/tutorsignup');
-    };
-    const home = () => {
-        navigate('/');
-    };
-    const back = () => {
-        navigate('/');
-    };
+  const navigate = useNavigate();
+  const tutorSignup = () => {
+    navigate("/tutorsignup");
+  };
+  const home = () => {
+    navigate("/");
+  };
+  const back = () => {
+    navigate("/");
+  };
 
-    // getter setter 
-    const[firstName, setFirstName] = useState("");
-    const[lastName, setLastName] = useState("");
-    const[name, setName] = useState("");
-    const[email, setEmail] = useState("");
-    const[password, setPassword] = useState("");
-    
-    // form function
-    const handleSignUp = (e) => {
-        e.preventDefault()
-        console.log(firstName,lastName,name,email,password);
-    };
+  // getter setter
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className="tutor-sign-up">
-            <div className="div">
-            <div className="overlap-group">
-            <img
-                className="green-rectangle"
-                alt="Green rectangle"
-                src={greenRectangleDesign}
-            />
-                {/*Add placeholders and input*/}
-            <div className="frame">
-                <div className="div-2">
-                    <div className="frame-2">
-                        <div className="text-wrapper">Student Sign Up</div>
+  // form function
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(firstName, lastName, name, email, password);
+  };
 
-                            <form onSubmit={handleSignUp}>
-                                <div className="frame-3">
-                                    <div className="frame-4">
-                                        
-                                        <input className="div-wrapper" 
-                                        placeholder="First Name"
-                                        type ="text"
-                                        value={firstName}
-                                        onChange={(e) => setFirstName(e.target.value)}
-                                        required
-                                        />
-                                        
-                                        <input className="div-wrapper" 
-                                        placeholder="Last Name"
-                                        type ="text"
-                                        value={lastName}
-                                        onChange={(e) => setLastName(e.target.value)}
-                                        required
-                                        />
+  return (
+    <div className="tutor-sign-up">
+      <div className="div">
+        <div className="overlap-group">
+          <img
+            className="green-rectangle"
+            alt="Green rectangle"
+            src={greenRectangleDesign}
+          />
+          {/*Add placeholders and input*/}
+          <div className="frame">
+            <div className="div-2">
+              <div className="frame-2">
+                <div className="text-wrapper">Student Sign Up</div>
 
-                                        <input
-                                        className="email"
-                                        placeholder="Email"
-                                        type="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                        />
+                <form onSubmit={handleSignUp}>
+                  <div className="frame-3">
+                    <div className="frame-4">
+                      <input
+                        className="div-wrapper"
+                        placeholder="First Name"
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                      />
 
-                                        <input className="div-wrapper" 
-                                        type ="text"
-                                        placeholder="Name"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                        required
-                                        />
+                      <input
+                        className="div-wrapper"
+                        placeholder="Last Name"
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                      />
 
-                                        <input className="div-wrapper" 
-                                        type ="password"
-                                        placeholder="Password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        required
-                                        />
-                                        
-                                    </div>
+                      <input
+                        className="email"
+                        placeholder="Email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
 
-                                    <button className="sign-in-button" type="submit">
-                                        <div className="text-wrapper-3">Sign Up</div>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                      <input
+                        className="div-wrapper"
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+
+                      <input
+                        className="div-wrapper"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
                     </div>
 
-                    <div className="frame-wrapper">
-                        <div className="frame-5">
-                            <div className="rectangle" />
-
-                            <div className="text-wrapper-4">or Login</div>
-
-                            <div className="rectangle" />
-                        </div>
-                    </div>
-
-                        <div className="div-2">
-                        <p className="p">Need a student account? Sign up here:</p>
-
-                            <div className="sign-up-buttons">
-                                <div className="tutor-button">
-                                    <button className="tutor" onClick={tutorSignup}>Tutor Sign Up</button>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                </div>
-
-                <div className="top-bar">
-                    <div className="back-logo">
-                        <button onClick={home}><img className="logo" alt="Logo" src={logo} /></button>
-                        <button onClick={back}>
-                            <img className="back-button" alt="Back button" src={backButton} />
-                        </button>
-                        
-                    </div>
-
-                        <p className="TUTOR-TITANS">
-                            <span className="span">TUTOR</span>
-
-                            <span className="text-wrapper-5">&nbsp;</span>
-
-                            <span className="text-wrapper-6">TITANS</span>
-                        </p>
-                </div>
+                    <button className="sign-in-button" type="submit">
+                      <div className="text-wrapper-3">Sign Up</div>
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
+
+            <div className="frame-wrapper">
+              <div className="frame-5">
+                <div className="rectangle" />
+
+                <div className="text-wrapper-4">or Login</div>
+
+                <div className="rectangle" />
+              </div>
+            </div>
+
+            <div className="div-2">
+              <p className="p">Need a student account? Sign up here:</p>
+
+              <div className="sign-up-buttons">
+                <div className="tutor-button">
+                  <button className="tutor" onClick={tutorSignup}>
+                    Tutor Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="top-bar">
+          <div className="back-logo">
+            <button onClick={home}>
+              <img className="logo" alt="Logo" src={logo} />
+            </button>
+            <button onClick={back}>
+              <img className="back-button" alt="Back button" src={backButton} />
+            </button>
+          </div>
+
+          <p className="TUTOR-TITANS">
+            <span className="span">TUTOR</span>
+
+            <span className="text-wrapper-5">&nbsp;</span>
+
+            <span className="text-wrapper-6">TITANS</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
