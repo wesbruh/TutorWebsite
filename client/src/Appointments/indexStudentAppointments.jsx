807571044336eba5641f43e3f73544085aa5efcf
TjMal <tmalizia@csus.edu>
2024-11-19 13:22:48 -0800

    Edited appointments .jsx and .css files. Also fixed errer in studentBilling.css

--- client/src/Appointments/indexStudentAppointments.jsx
+++ client/src/Appointments/indexStudentAppointments.jsx
@@ -3,9 +3,14 @@ import Sidebar from "../components/Sidebar";
 import "./StudentAppointments.css";
 
 function StudentAppointments () {
+
     return (
-        <div className="appointmentsPage">
+        <div className="appointments-page">
             <Sidebar />
+            <main className="header">
+                <h1>Appointments</h1>
+                <p>View and schedule a tutoring appointment.</p>
+                </main>
         </div>
     );
 };
