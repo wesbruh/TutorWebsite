bd8955361e2454663fa92f49947afb17c665718d
TMaliz <tmalizia@csus.edu>
2024-11-24 21:14:39 -0800

    Updated student dashboard.

--- client/src/Dashboard/indexStudentDashboard.jsx
+++ client/src/Dashboard/indexStudentDashboard.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import "./StudentDashboard.css";
+import { useAuth } from "../context/auth";
+//import user controller
 
 function StudentDashboard () {
+   
+    // get name of user
+    // const [name, getName] = useState("");
+
+    // useEffect(() => ){
+    //     async function fetchName(){
+    //         try{
+    //             const response = await fetch()
+    //         }
+    //     }
+    // } //Just messing with some things...Will likely scrap this approach...I think we need a separate thing to control calls to the database...
+
     return (
         <div className="dashboard">
             <Sidebar />
+            <main className="main-content">
+                <h1>Hello, NAME WILL GO HERE.</h1>
+                <p>Welcome to your student dashboard.</p>
+                <div className="appointments">
+                    <h2>Upcoming Sessions</h2>
+                 </div> 
+                    <div className ="oldAppointments">
+                        <h2>Past Sessions</h2>
+                </div>
+                <div className="billing">
+                    <h2>Balance Due: $</h2>
+                </div>
+                </main>
         </div>
+       
+        
     );
 };
 
