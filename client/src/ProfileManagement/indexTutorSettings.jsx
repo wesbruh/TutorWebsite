dfa076584da8a410aa0a8c1cf9a0e093034db266
sb11-1 <sbajwa2@csus.edu>
2024-11-27 20:41:25 -0800

    added to tutor settings page based on student settings page

--- client/src/ProfileManagement/indexTutorSettings.jsx
+++ client/src/ProfileManagement/indexTutorSettings.jsx
@@ -268,3 +268,76 @@ export const TutorSettingsPage = () => {
         </div>
     );
 };
+/*
+//using code from student setting with adjustments
+import React, {useState} from "react";
+import Sidebar from "../components/Sidebar";
+import "./styleTutorSettings.css";
+import axios from "axios";
+
+export const TutorSettings = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const UpdateInfo = async (e) => { };
+
+    return (
+        <div className="tutor-settings">
+            <Sidebar />
+            <main className="main-content">
+                <h1>Account Settings</h1>
+                <p>Change your profile picture, bio, and settings.</p>
+                <form onSubmit={UpdateInfo}>
+                <div className="input-fields">
+                    <div className = "input-item">
+                        <div>
+                        <p className="title">Enter First Name</p>
+                        <input 
+                        className="input" 
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        </div>
+                    </div>
+                    <div className = "input-item">
+                        <div>
+                        <p className="title">Enter Last Name</p>
+                        <input 
+                        className="input" 
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        />
+                        </div>
+                    </div>
+                    <div className = "input-item">
+                        <div>
+                        <p className="title">Enter Username</p>
+                        <input 
+                        className="input" 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                        </div>
+                    </div>
+                    <div className = "input-item">
+                        <div>
+                        <p className="title">Enter Email</p>
+                        <input 
+                        className="input" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
+                        </div>
+                    </div>
+                    <button className="input" type="submit">Submit</button>
+                </div>
+                </form>
+            </main>
+        </div>
+    );
+};
+
+
+*/
