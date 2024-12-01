3aa3fe3a0dfe3d495854f9552765005ebbf79fc7
sb11-1 <sbajwa2@csus.edu>
2024-11-30 23:38:29 -0800

    remove buggy code from payroll

--- client/src/Payroll/indexTutorPayroll.jsx
+++ client/src/Payroll/indexTutorPayroll.jsx
@@ -17,25 +17,8 @@ const TutorPayrollPage = () => {
     .catch(err => console.log(err))
   }, [])
 
-  
-  
   return (
-    /*<div className="payroll-page">
-      <Sidebar />
-      <main className="main-content">
-        <h1>Payroll</h1>
-        <p>View Your Billing Information.</p>
-        <ul className="payroll-list">
-          {payroll.map((payroll) => (
-            <li key={payroll.id} className="payroll-item">
-              <div className="payroll-icon"></div>
-              <div>
-                <p className="payroll-title">{payroll.title}</p>
-                <p className="payroll-date">{payroll.date}</p>
-                <p className="payroll-amount"> Total Amount: ${payroll.amount}</p>
-              </div>
-            </li>
-          ))}*/
+
          <div className = "payroll-page">
           <Sidebar />
             {tutors.length === 0 ? (
