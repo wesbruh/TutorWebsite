84cbb0d91fe63461e071d2c79581b55e3f6f28a2
aapruett <apruett001@gmail.com>
2024-12-02 15:02:55 -0800

    displaying email on dashboard; working to display name

--- client/src/Dashboard/indexStudentDashboard.jsx
+++ client/src/Dashboard/indexStudentDashboard.jsx
@@ -1,25 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Sidebar from "../components/StudentSideBar/Sidebar";
 import "./StudentDashboard.css";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 //import user controller
 
 function StudentDashboard() {
+  const thename = JSON.parse(localStorage.getItem("auth"));
   // get name of user
-  // const [name, getName] = useState("");
-
-  // useEffect(() => ){
-  //     async function fetchName(){
-  //         try{
-  //             const response = await fetch()
-  //         }
-  //     }
-  // } //Just messing with some things...Will likely scrap this approach...I think we need a separate thing to control calls to the database...
-
+  const name = useState(thename.user.email);
+  
   return (
     <div className="dashboard">
       <Sidebar />
       <main className="main-content">
-        <h1>Hello, Billy Joel.</h1>
+        <h1>Hello, {name} </h1>
         <p>Welcome to your student dashboard.</p>
         <div className="appointments">
           <h2>Upcoming Sessions</h2>
