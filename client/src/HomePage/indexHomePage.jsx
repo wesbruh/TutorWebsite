722f73e967db51f3213e6e8cab3cc0ab8ddb8a99
wesbruh <westleyv77@gmail.com>
2024-12-02 17:08:55 -0800

    fixed home page. now properly uses hometutorcards, and scales. login/signup page untouched.

--- client/src/HomePage/indexHomePage.jsx
+++ client/src/HomePage/indexHomePage.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom"; // import routes
 import logo from "../assets/logo.svg"; // titans logo
 import "./indexHomePage.css"; // HomePage.css
 import  {HomeTutorCard}  from "../components/TutorCards/HomeTutorCard";
+import tutorcard1PersonaPhoto from "../assets/tutorcard-1-persona-photo.png"
+import tutorcard2PersonaPhoto from "../assets/tutorcard-2-persona-photo.png"
+import tutorcard3PersonaPhoto from "../assets/tutorcard-3-persona-photo.png"
 
 export const Home = () => {
   const navigate = useNavigate();
@@ -34,11 +37,30 @@ export const Home = () => {
       <h3>Learning Made Simple<br/>
           Success Made Possible
       </h3>
-      <HomeTutorCard 
+      <div className="tutor-cards">
+      <HomeTutorCard
+        imgSrc={tutorcard1PersonaPhoto}
         imgAlt="Tutor1 Image" 
         title="Jane Smith"
         subject="Math" 
+        rating = "5/5"
         description="My goal is to build relationships with each student that I come across utilizing my empathy, emotion, and values shared in my work."/>
+        <HomeTutorCard
+        imgSrc={tutorcard2PersonaPhoto}
+        imgAlt="Tutor2 Image" 
+        title="Ava Lee"
+        subject="Biology"
+        rating = "4/5"
+        description="My goal is to build relationships with each student that I come across utilizing my empathy, emotion, and values shared in my work."/>
+        <HomeTutorCard
+        imgSrc={tutorcard3PersonaPhoto}
+        imgAlt="Tutor3 Image" 
+        title="Eleanor Brooks"
+        subject="English"
+        rating = "4/5"
+        description="My goal is to build relationships with each student that I come across utilizing my empathy, emotion, and values shared in my work."/>
+      </div>
+      
     </div>
   );
 };
